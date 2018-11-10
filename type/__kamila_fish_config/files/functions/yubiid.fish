7faa1c4a0aed729ddcaa9677b3f43b8c71555f00
Kamila Souckova <kamila@ksp.sk>
2018-11-10 08:57:02 +0100

    [fish config] add function for getting the SSH public key from yubikey

---
+++ type/__kamila_fish_config/files/functions/yubiid.fish
@@ -0,0 +1,10 @@
+function yubiid -d "Cat's the SSH key stored on the yubikey (assuming an agent is running)"
+    ssh-add -L | grep cardno
+    if [ -n "$DISPLAY" ]
+        if which xclip >/dev/null
+            ssh-add -L | grep cardno | xclip -selection clipboard -i ; and echo "[copied to clipboard]" >&2
+        else if which xsel >/dev/null
+            echo '[TODO xsel support needed]' >&2
+        end
+    end
+end
